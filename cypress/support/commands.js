10e84e6208782169110546cb4953e50d9d14c9d3
Gabriel Bento <gabrielbento299@hotmail.com>
2023-08-28 16:10:45 -0300

    session four: exercice extra two

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,3 +15,16 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (emailAddress, password) => {
     cy.wait('@getNotes');
   });
 });
+
+Cypress.Commands.add('login', (
+  userName = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+
+  cy.visit('/login');
+  cy.get('#email').type(userName);
+  cy.get('#password').type(password, { log: false });
+  cy.contains('button', 'Login').click();
+
+  cy.contains('h1', 'Your Notes').should('be.visible');
+});
