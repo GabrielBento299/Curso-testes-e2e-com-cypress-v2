0918e2a52a49148dffff6a9b0a6ddad00a51a4ff
Gabriel Bento <gabrielbento299@hotmail.com>
2023-08-30 16:53:36 -0300

    session six: Exercice Testing filling out the credit card form

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     requestTimeout: 10000,
-    video: false
+    video: false,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -39,9 +39,7 @@ Cypress.Commands.add('sessionLogin', (
   cy.session(userName, login);
 });
 
-const attachFileHandlear = () => {
-  cy.get('#file').selectFile('cypress/fixtures/example.json');
-};
+const attachFileHandlear = () => cy.get('#file').selectFile('cypress/fixtures/example.json');
 
 Cypress.Commands.add('createNote', (noteDescription, attachFile = false) => {
   cy.visit('/notes/new');
@@ -55,7 +53,7 @@ Cypress.Commands.add('createNote', (noteDescription, attachFile = false) => {
   cy.contains('.list-group-item', noteDescription).should('be.visible');
 });
 
-Cypress.Commands.add('editNote', (noteDescription, updatedNoteDescription, attachFile = false) => {
+Cypress.Commands.add('editNote', (noteDescription, newNoteValue, attachFile = false) => {
   cy.intercept('GET', '**/notes/**').as('getNote');
 
   cy.contains('.list-group-item', noteDescription).click();
@@ -65,7 +63,7 @@ Cypress.Commands.add('editNote', (noteDescription, updatedNoteDescription, attac
     .as('contentField')
     .clear();
   cy.get('@contentField')
-    .type(updatedNoteDescription);
+    .type(newNoteValue);
 
   if (attachFile) {
     attachFileHandlear();
@@ -73,17 +71,39 @@ Cypress.Commands.add('editNote', (noteDescription, updatedNoteDescription, attac
 
   cy.contains('button', 'Save').click();
 
-  cy.contains('.list-group-item', updatedNoteDescription).should('be.visible');
+  cy.contains('.list-group-item', newNoteValue).should('be.visible');
   cy.contains('.list-group-item', noteDescription).should('not.exist');
 });
 
-Cypress.Commands.add('deleteNote', (updatedNoteDescription) => {
-  cy.contains('.list-group-item', updatedNoteDescription).click();
+Cypress.Commands.add('deleteNote', (newNoteValue) => {
+  cy.contains('.list-group-item', newNoteValue).click();
   cy.contains('button', 'Delete').click();
 
   cy.get('.list-group-item')
     .its('length')
     .should('be.at.least', 1);
-  cy.contains('.list-group-item', updatedNoteDescription)
+  cy.contains('.list-group-item', newNoteValue)
     .should('not.exist');
+});
+
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings');
+  cy.get('#storage').type('1');
+  cy.get('#name').type('Mary Doe');
+
+
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242');
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271');
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123');
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345');
+  cy.contains('button', 'Purchase').click();
 });
