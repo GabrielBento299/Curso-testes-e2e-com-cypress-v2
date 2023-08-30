526f89bf0c1bb3c4f1bc2eeca98a8aa32d43c168
Gabriel Bento <gabrielbento299@hotmail.com>
2023-08-30 15:02:58 -0300

    session five: Exercice testing CRUD

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    requestTimeout: 10000
+    requestTimeout: 10000,
+    video: false
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
